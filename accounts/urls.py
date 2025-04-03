24d75835e12098a1a3862bd9d62d9b938271678f
megadot99 <mubarakmuritala93@gmail.com>
2025-04-03 11:47:43 +0100

    initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+"""defines URL pattern for accounts"""
+from django.urls import path, include
+from . import views
+
+app_name = "accounts"
+urlpatterns = [
+    path('',include('django.contrib.auth.urls')),
+    # Registration page
+    path('register/',views.register,name='register'),
+] 
